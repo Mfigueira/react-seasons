21bdbfd9b7b1225874188093e13e02caf989d486
Manuel Figueira <manuel.figueira.9@gmail.com>
2021-07-15 16:36:23 -0300

    project update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import SeasonDisplay from './components/SeasonDisplay';
+import Spinner from './components/Spinner';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  // Babel transpiles this and generates the constructor for us
+  state = {
+    lat: null,
+    errorMessage: '',
+  };
+
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  componentDidMount() {
+    console.log('My component did mount');
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  componentDidUpdate() {
+    console.log('My component did update');
+  }
+
+  componentWillUnmount() {
+    console.log('Cleanup - component will unmount');
+  }
+
+  render() {
+    return (
+      <div>
+        {!this.state.lat && !this.state.errorMessage && (
+          <Spinner message="Please accept location request" />
+        )}
+
+        {this.state.lat && !this.state.errorMessage && (
+          <SeasonDisplay lat={this.state.lat} />
+        )}
+
+        {!this.state.lat && this.state.errorMessage && (
+          <h1>Error: {this.state.errorMessage}</h1>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/SeasonDisplay.js
@@ -0,0 +1,35 @@
+import './SeasonDisplay.css';
+
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach!",
+    icon: 'sun',
+  },
+  winter: {
+    text: "Burr, it's chilly!",
+    icon: 'snowflake',
+  },
+};
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    return lat > 0 ? 'winter' : 'summer';
+  }
+};
+
+const SeasonDisplay = ({ lat }) => {
+  const season = getSeason(lat, new Date().getMonth());
+  const { text, icon } = seasonConfig[season];
+
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${icon} icon`} />
+      <h1>{text}</h1>
+      <i className={`icon-right massive ${icon} icon`} />
+    </div>
+  );
+};
+
+export default SeasonDisplay;
